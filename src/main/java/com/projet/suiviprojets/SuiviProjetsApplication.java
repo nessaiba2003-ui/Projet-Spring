0a624f68081955cf68db01a118a33bb25381c757
HP PRO <hp.pro@email.com>
2026-03-12 22:12:59 +0000

    Initial commit: Spring Boot project structure
    # Conflicts:
    #	pom.xml
    #	src/main/java/com/projet/suiviprojets/SuiviProjetsApplication.java
    #	src/main/resources/application.properties
    #	src/test/java/com/projet/suiviprojets/SuiviProjetsApplicationTests.java

--- src/main/java/com/projet/suiviprojets/SuiviProjetsApplication.java
+++ src/main/java/com/projet/suiviprojets/SuiviProjetsApplication.java
@@ -6,8 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SuiviProjetsApplication {
 
+<<<<<<< HEAD
     public static void main(String[] args) {
         SpringApplication.run(SuiviProjetsApplication.class, args);
     }
+=======
+	public static void main(String[] args) {
+		SpringApplication.run(SuiviProjetsApplication.class, args);
+	}
+>>>>>>> 2a5b38b (Initial commit: Spring Boot project structure)
 
 }
--- src/test/java/com/projet/suiviprojets/SuiviProjetsApplicationTests.java
+++ src/test/java/com/projet/suiviprojets/SuiviProjetsApplicationTests.java
@@ -6,8 +6,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SuiviProjetsApplicationTests {
 
+<<<<<<< HEAD
     @Test
     void contextLoads() {
     }
+=======
+	@Test
+	void contextLoads() {
+	}
+>>>>>>> 2a5b38b (Initial commit: Spring Boot project structure)
 
 }
