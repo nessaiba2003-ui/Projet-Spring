c86bac4142692fb6b0ce7eac0c6c75fb67df1c47
Hafsa <250507787+Hafsa-Belahnech@users.noreply.github.com>
2026-03-12 21:20:23 +0000

    first

---
+++ src/main/java/com/projet/suiviprojets/SuiviProjetsApplication.java
@@ -0,0 +1,13 @@
+package com.projet.suiviprojets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuiviProjetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuiviProjetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projet/suiviprojets/SuiviProjetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.suiviprojets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuiviProjetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
